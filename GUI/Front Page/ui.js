bb8af6598b174a9b1be87aa966f041232cbb6737
jmario2000 <113378375+jmario2000@users.noreply.github.com>
2023-01-29 12:20:18 -0500

    Create ui.js
    Login and Create Account buttons' functionality

---
+++ GUI/Front Page/ui.js
@@ -0,0 +1,14 @@
+/* Functionaliy for Login and Create Account Buttons */
+
+function showLogin() {
+    document.getElementById("homepage").style.display = "none";
+    document.getElementById("create-account").style.display = "none";
+    document.getElementById("login").style.display = "block";
+  }
+  
+  function showCreateAccount() {
+    document.getElementById("homepage").style.display = "none";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("create-account").style.display = "block";
+  }
+  
