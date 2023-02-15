2e5d4d0aa4981451d88a6fe3a17e178f4b410fde
AnthonyLupica <arl127@uakron.edu>
2023-02-14 19:34:09 -0500

    Adding some documentation and a new favicon

--- sentimento/frontend/src/App.js
+++ sentimento/frontend/src/App.js
@@ -1,3 +1,9 @@
+/*
+ * app.js is the main component of the application, which serves as a container for other components. 
+ * Responsible for the logic of the overall structure and behavior of the application, 
+ * and will include routing and state management
+ */
+
 import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import JournalEntry from "./components/JournalEntry";
