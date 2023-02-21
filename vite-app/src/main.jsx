2521a569d194175aa62ecda679f0697747ec6bdb
Anthony Lupica <97695540+AnthonyLupica@users.noreply.github.com>
2023-02-20 23:27:47 -0500

    Update main.jsx

--- vite-app/src/main.jsx
+++ vite-app/src/main.jsx
@@ -1,3 +1,8 @@
+/*
+    main.jsx renders the parent component "App",
+    and is the entry point for the react app
+*/
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
